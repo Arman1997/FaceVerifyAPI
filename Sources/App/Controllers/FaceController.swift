4addaff1c50bf8add36ccdaaf1876a1695759a7b
Arman Galstyan <armangalstyan9597@gmail.com>
2017-12-14 04:44:49 +0400

    implement imageSaving and creating new person

--- Sources/App/Controllers/FaceController.swift
+++ Sources/App/Controllers/FaceController.swift
@@ -16,10 +16,41 @@ class FaceController {
     }
     
     func saveNewFaces(_ req: Request) throws -> ResponseRepresentable {
-        return ""
+        guard let imagePaths = req.json?.wrapped["imagePaths"]?.pathIndexableObject?.map({ $0.value.string ?? "" }) else {
+            throw Abort.badRequest
+        }
+       
+        let newPersonId = UUID().uuidString
+        var faceIdsArray = [String]()
+        try imagePaths.forEach({ (imagePath) in
+            let newFaceId = UUID().uuidString
+            let newFace = Face(faceId: newFaceId, personId: newPersonId, imagePath: imagePath)
+            try newFace.save()
+            faceIdsArray.append(newFaceId)
+        })
+        
+        let newCreatedPerson = NewCreatedPerson(personId: newPersonId, faceIdsArray: faceIdsArray)
+        return newCreatedPerson
     }
     
 }
 
+
+struct NewCreatedPerson: ResponseRepresentable,JSONRepresentable {
+    struct Keys {
+        static let personId = "personId"
+        static let faceIds = "faceIds"
+    }
+    
+    var personId: String
+    var faceIdsArray: [String]
+    
+    func makeJSON() throws -> JSON {
+        var json = JSON()
+        try json.set(Keys.personId, self.personId)
+        try json.set(Keys.faceIds, self.faceIdsArray)
+        return json
+    }
+}
 
 
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 extension Droplet {
     func setupRoutes() throws {
@@ -6,5 +7,19 @@ extension Droplet {
         let faceController = FaceController()
         get("faces",handler: faceController.getAllFaces)
         post("faces", handler: faceController.saveNewFaces)
+        
+        post("faceImage") { request in
+            guard let imageDataBytes = request.data["faceImage"]?.bytes else {
+                throw Abort.badRequest
+            }
+            // detect face
+            // write array in text file
+            let imageData = Data(bytes: imageDataBytes)
+            var imagePath = ""
+            try FileCachingManager.sharedInstance.saveImage(withData: imageData) { (savedImagePath) in
+                imagePath = savedImagePath
+            }
+            return imagePath
+        }
     }
 }
